5aa886e0be698f6d926f7799f4ed95d07f833540
ashmerchuk24 <115457689+ashmerchuk24@users.noreply.github.com>
2023-03-22 11:25:16 +0100

    start

---
+++ src/main/java/com/example/eventsCalendert/EventsCalendertApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventsCalendert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsCalendertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsCalendertApplication.class, args);
+	}
+
+}
